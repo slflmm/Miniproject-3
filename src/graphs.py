d3cde7ec317f2266eb0646e87f6190f243ddc9d7
Stephanie Laflamme <stephanie.laflamme@mail.mcgill.ca>
2014-10-24 17:57:15 -0400

    Added plotting gabor filters

--- src/graphs.py
+++ src/graphs.py
@@ -1,132 +1,192 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.pylab import *
+from features import *
 
 # ------------------
 # PERCEPTRON RESULTS
 # ------------------
+class PerceptronPlotter(object):
 
-# these were the parameters considered
-alphas = np.array([0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001])
-niters = np.array([10, 15, 20, 25, 30, 35])
+	def __init__(self):
+		# these were the parameters considered
+		self.alphas = np.array([0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001])
+		self.niters = np.array([10, 15, 20, 25, 30, 35])
 
-# load crossval results
-crossval_results = np.load('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/src/results/perceptron_crossval_results.npy')
+		# load crossval results
+		self.crossval_results = np.load('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/results/perceptron_crossval_results.npy')
 
-crossval_matrices = np.load('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/src/results/perceptron_crossval_confmatrices.npy')
+		self.crossval_matrices = np.load('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/results/perceptron_crossval_confmatrices.npy')
 
-crossval_training = np.load('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/src/results/perceptron_crossval_training_accuracy.npy')
+		self.crossval_training = np.load('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/results/perceptron_crossval_training_accuracy.npy')
 
-# ------------
-# Best results
-# ------------
-argmax = np.argmax(crossval_results)
-best = np.max(crossval_results)
-best_alpha = alphas[argmax % 7]
-best_niters = niters[argmax / 7]
-print 'Best PERCEPTRON: '
-print 'accuracy = %f' % best
-print 'alpha = %f' % best_alpha
-print 'n_iters = %d' % best_niters
+		self.argmax = np.argmax(self.crossval_results)
+		self.best = np.max(self.crossval_results)
+		self.best_alpha = self.alphas[self.argmax % 7]
+		self.best_niters = self.niters[self.argmax / 7]
 
-# -------------------------------- 
-# Perceptron's crossval gridsearch
-# --------------------------------
-accuracies = crossval_results.reshape((len(niters),len(alphas)))
-
-fig = plt.figure(figsize=(7,6))
-ax1 = fig.add_subplot(111)
-
-ax1.tick_params(direction='out', which='both')
-ax1.set_xlabel('Learning rate')
-ax1.set_ylabel('Number of iterations')
-ax1.set_xticks(alphas)
-ax1.set_yticks(niters)
-
-cax = ax1.contourf(alphas, niters, accuracies, np.arange(21, 27, 0.5), extend='both')
-# cs=ax1.contour(alphas, niters, accuracies, np.arange(20,27,0.25),colors='k')
-# ax1.clabel(cs, fmt = '%d', colors = 'k')
-
-ax1.set_xscale('log')
-
-cbar = fig.colorbar(cax)
-plt.savefig('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/report/perceptron_gridsearch.pdf')
+	def best_results(self):
+		print 'Best PERCEPTRON: '
+		print 'accuracy = %f' % self.best
+		print 'alpha = %f' % self.best_alpha
+		print 'n_iters = %d' % self.best_niters
 
 
-# -----------------------------
-# Perceptron's confusion matrix
-# -----------------------------
+	def gridsearch(self):
+		accuracies = self.crossval_results.reshape((len(self.niters),len(self.alphas)))
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+		fig = plt.figure(figsize=(7,6))
+		ax1 = fig.add_subplot(111)
 
-mat = crossval_matrices[argmax].tolist()
-cax = ax.matshow(mat, cmap=cm.jet)
-fig.colorbar(cax)
+		ax1.tick_params(direction='out', which='both')
+		ax1.set_xlabel('Learning rate')
+		ax1.set_ylabel('Number of iterations')
+		ax1.set_xticks(self.alphas)
+		ax1.set_yticks(self.niters)
 
-for x in xrange(10):
-	for y in xrange(10):
-		ax.annotate('%4.2f' % (mat[x][y]), xy=(y,x), horizontalalignment='center', verticalalignment='center', color='white')
+		cax = ax1.contourf(self.alphas, self.niters, accuracies, np.arange(22, 27, 0.25), extend='both')
+		# cs=ax1.contour(alphas, niters, accuracies, np.arange(20,27,0.25),colors='k')
+		# ax1.clabel(cs, fmt = '%d', colors = 'k')
 
-plt.xticks(np.arange(10))
-plt.yticks(np.arange(10))
-ax.set_title('True label', fontsize=16)
-ax.set_ylabel('Prediction', fontsize=16)
+		ax1.set_xscale('log')
 
-plt.savefig('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/report/perceptron_confusion.pdf')
-
-# -------------------------------------------------------- 
-# Perceptron's learning rate -- validation vs training set
-# (using the best number of iterations, vary learning rate)
-# --------------------------------------------------------
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-
-valid_acc = crossval_results[(argmax/7)*7:(argmax/7)*7 + 7]
-train_acc = crossval_training[(argmax/7)*7:(argmax/7)*7 + 7]
-
-v, = ax.plot(alphas, valid_acc, marker='D', color='green', label='Validation, %d iterations' % best_niters)
-t, = ax.plot(alphas, train_acc, marker='D', color='green', linestyle=':', label='Training, %d iterations' % best_niters)
-
-# legend
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles, labels, loc=0)
-
-# labels
-ax.set_xlabel('Learning rate')
-ax.set_ylabel('Mean accuracy')
-ax.set_xscale('log')
-
-plt.savefig('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/report/perceptron_learningrate.pdf')
-
-# -------------------------------------------------------- 
-# Perceptron's iterations -- validation vs training set
-# (using the best learning rate, vary iterations)
-# --------------------------------------------------------
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-
-valid_acc = []
-for i in xrange(len(niters)):
-	valid_acc.append(crossval_results[7*i + argmax%7])
-train_acc = []
-for i in xrange(len(niters)):
-	train_acc.append(crossval_training[7*i + argmax%7])
-
-v, = ax.plot(niters, valid_acc, marker='D', color='green', label='Validation, alpha=%5.4f' % best_alpha)
-t, = ax.plot(niters, train_acc, marker='D', color='green', linestyle=':', label='Training, alpha=%5.4f' % best_alpha)
-
-# legend
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles, labels, loc=0)
-
-# labels
-ax.set_xlabel('Number of iterations')
-ax.set_ylabel('Mean accuracy')
-
-plt.savefig('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/report/perceptron_iterations.pdf')
+		cbar = fig.colorbar(cax)
+		plt.savefig('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/report/perceptron_gridsearch.pdf')
 
 
+	def conf_matrix(self):
+		fig = plt.figure()
+		ax = fig.add_subplot(111)
+
+		mat = self.crossval_matrices[self.argmax].tolist()
+		cax = ax.matshow(mat, cmap=cm.jet)
+		fig.colorbar(cax)
+
+		for x in xrange(10):
+			for y in xrange(10):
+				ax.annotate('%4.2f' % (mat[x][y]), xy=(y,x), horizontalalignment='center', verticalalignment='center', color='white')
+
+		plt.xticks(np.arange(10))
+		plt.yticks(np.arange(10))
+		ax.set_title('True label', fontsize=16)
+		ax.set_ylabel('Prediction', fontsize=16)
+
+		plt.savefig('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/report/perceptron_confusion.pdf')
+
+
+	def plt_learningrate(self):
+		'''
+		Perceptron's learning rate -- validation vs training set 
+		Uses the best n_iter, varies learning rate
+		'''
+		fig = plt.figure()
+		ax = fig.add_subplot(1,1,1)
+
+		crossval_results = self.crossval_results
+		crossval_training = self.crossval_training
+		argmax = self.argmax
+
+		valid_acc = crossval_results[(argmax/7)*7:(argmax/7)*7 + 7]
+		train_acc = crossval_training[(argmax/7)*7:(argmax/7)*7 + 7]
+
+		v, = ax.plot(self.alphas, valid_acc, marker='D', color='green', label='Validation, %d iterations' % self.best_niters)
+		t, = ax.plot(self.alphas, train_acc, marker='D', color='green', linestyle=':', label='Training, %d iterations' % self.best_niters)
+
+		# legend
+		handles, labels = ax.get_legend_handles_labels()
+		ax.legend(handles, labels, loc=0)
+
+		# labels
+		ax.set_xlabel('Learning rate')
+		ax.set_ylabel('Mean accuracy')
+		ax.set_xscale('log')
+
+		plt.savefig('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/report/perceptron_learningrate.pdf')
+
+
+	def plt_niters(self):
+		'''
+		Perceptron's number of iterations -- validation vs training set 
+		Uses the best alpha, varies number of iterations.
+		'''
+		fig = plt.figure()
+		ax = fig.add_subplot(1,1,1)
+
+		crossval_results = self.crossval_results
+		crossval_training = self.crossval_training
+		argmax = self.argmax
+
+		valid_acc = []
+		for i in xrange(len(self.niters)):
+			valid_acc.append(crossval_results[7*i + argmax%7])
+		train_acc = []
+		for i in xrange(len(self.niters)):
+			train_acc.append(crossval_training[7*i + argmax%7])
+
+		v, = ax.plot(self.niters, valid_acc, marker='D', color='green', label='Validation, alpha=%5.4f' % self.best_alpha)
+		t, = ax.plot(self.niters, train_acc, marker='D', color='green', linestyle=':', label='Training, alpha=%5.4f' % self.best_alpha)
+
+		# legend
+		handles, labels = ax.get_legend_handles_labels()
+		ax.legend(handles, labels, loc=0)
+
+		# labels
+		ax.set_xlabel('Number of iterations')
+		ax.set_ylabel('Mean accuracy')
+
+		plt.savefig('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/report/perceptron_iterations.pdf')
+
+	def all_plots(self):
+		'''
+		Builds all plots and prints all info.
+		'''
+		self.best_results()
+		self.gridsearch()
+		self.conf_matrix()
+		self.plt_niters()
+		self.plt_learningrate()
+
+
+# ---------------------
+# GABOR FILTER EXAMPLES
+# ---------------------
+class GaborPlotter(object):
+
+	def __init__(self):
+		self.n_theta = 4
+		self.sigmas=[1,3]
+		self.frequencies=[0.05, 0.25]
+		self.kernels = getGaborKernels(self.n_theta, self.sigmas, self.frequencies)
+
+	def show_samples(self):
+		fig = plt.figure()
+		for i in range(4):
+			plt.subplot(2,2,i+1)
+			plt.imshow(np.real(self.kernels[i + 4*i]), cmap=plt.cm.gray, interpolation='nearest')
+			# the loop goes theta in range n_theta, then sigmas, then frequencies
+			# theta = (i%4)*1. / self.n_theta
+			if (i==1):
+				plt.title(r'$\theta=\pi/%d$, $\sigma=%1.f$, $freq=%3.2f$' % (self.n_theta, self.sigmas[i/2], self.frequencies[i%2]), fontsize=16)
+			elif (i==2):
+				plt.title(r'$\theta=\pi/%d$, $\sigma=%1.f$, $freq=%3.2f$' % (self.n_theta/2, self.sigmas[i/2], self.frequencies[i%2]), fontsize=16)
+			elif (i != 0):
+				plt.title(r'$\theta=%d\pi/%d$, $\sigma=%1.f$, $freq=%3.2f$' % (i%4, self.n_theta, self.sigmas[i/2], self.frequencies[i%2]), fontsize=16)
+			else:
+				plt.title(r'$\theta=0$, $\sigma=%1.f$, $freq=%3.2f$' % (self.sigmas[i/2], self.frequencies[i%2]), fontsize=16)
+
+			plt.gca().xaxis.set_major_locator(plt.NullLocator())
+			plt.gca().yaxis.set_major_locator(plt.NullLocator())
+
+		plt.savefig('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/report/gabors.pdf')
+
+		plt.close()
+
+
+# ------------------------------------------
+# Do the actual graphing of your choice here
+# ------------------------------------------
+gbp = PerceptronPlotter()
+gbp.all_plots()
 
 
 
