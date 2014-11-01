bc4e42962f3d5c9db349b793e2bdaa83a605e689
Stephanie Laflamme <stephanie.laflamme@mail.mcgill.ca>
2014-10-31 22:13:38 -0400

    Testing

--- src/part4.py
+++ src/part4.py
@@ -71,9 +71,10 @@ for data in CrossValidation(examples, categories, k=10):
 		# after (20, 1, 5, 5) images are (48-5+1 = 44) --> 22 x 22, then (22-5+1 = 18) --> 9x9, then... 
 		# (48-9+1=40) => 20x20, then (20-5+1 = 16)=> 8, then (8-5+1=4)=> 2
 		# (48-7+1 = 42) => 21x21, then (21-6+1=16)=> 8x8, then (8-4+1=5)=> 5x5, and finally (5-3+1)=> 3x3
-		conv_filter_shapes = [(32, 1, 7, 7), (64, 32, 6, 6),[80, 64, 4, 4]],#, [96, 80, 3, 3]], #(22, 22) output, shape ()
-		image_shapes = [(batch_size, 1,48,48),(batch_size, 32, 21, 21), (batch_size, 64, 8, 8)],#, (batch_size, 80, 5, 5)], # (9, 9) output, shape (20,50,22,22) #80*2*2=320 but not getting that
-		poolsizes=[(2,2),(2,2), None],
+		# 21x21, then 16x16, (16-5+1=12) 12x12
+		conv_filter_shapes = [(32, 1, 7, 7), (64, 32, 6, 6),(80, 64, 5, 5), (80,80,5,5)],#, [96, 80, 3, 3]], #(22, 22) output, shape ()
+		image_shapes = [(batch_size, 1,48,48),(batch_size, 32, 21, 21), (batch_size, 64, 16, 16)],#, (batch_size, 80, 5, 5)], # (9, 9) output, shape (20,50,22,22) #80*2*2=320 but not getting that
+		poolsizes=[(2,2),None, None,None],
 		hidden_layer_sizes=[200],
 		n_outputs=10,
 		learning_rate=learning_rate,
