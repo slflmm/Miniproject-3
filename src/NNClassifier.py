3a1683e6615e0671b3fea6fa2e017fe742815509
aghakazem <naghakazem@gmail.com>
2014-11-01 01:51:26 -0700

    update

--- src/NNClassifier.py
+++ src/NNClassifier.py
@@ -18,11 +18,11 @@ class NeuralNetworkClassifier:
 
         self._inputLayer = []
         self._outputLayer = []
-        self._prevDeltaWeight = []
+        #self._prevDeltaWeight = []
 
         for (x, y) in zip(layerSize[:-1], layerSize[1:]):
-           self.weights.append(np.random.normal(scale=0.1, size = (y, x))) #initialize all weights to small random number
-           self._prevDeltaWeight.append(np.zeros((y, x)))
+           self.weights.append(np.random.normal(scale=0.1, size = (y, x+1))) #initialize all weights to small random number
+           #self._prevDeltaWeight.append(np.zeros((y, x+1)))
 
         lFuncs = []
         for i in range(self.layerCount):
@@ -35,15 +35,16 @@ class NeuralNetworkClassifier:
 
     "----- Compute the output of all units in the network -----"
     def FeedForward(self, input):
+        nSamples = input.shape[0]
 
         self._inputLayer = []
         self._outputLayer = []
 
         for index in range(self.layerCount):
             if index == 0:
-                inputLayer = self.weights[0].dot(input.T)
+                inputLayer = self.weights[0].dot(np.vstack([input.T, np.ones([1, nSamples])]))
             else:
-                inputLayer = self.weights[index].dot(self._outputLayer[-1])
+                inputLayer = self.weights[index].dot(np.vstack([self._outputLayer[-1], np.ones([1, nSamples])]))
 
             self._inputLayer.append(inputLayer)
             "Compute output using appropriate transfer function"
@@ -51,9 +52,10 @@ class NeuralNetworkClassifier:
 
         return self._outputLayer[-1].T
 
-    "----- Learning Function: back propagation and gradient descent -----"
-    def Learning(self, input, target, alpha = 0.1, beta = 0.5):
+    "----- TrainingEpoch: back propagation and gradient descent -----"
+    def TrainingEpoch(self, input, target, lenMiniBatch, alpha = 0.1, lmbda = 0.0):#beta = 0.5):
         delta = []
+        nSamples = input.shape[0]
 
         yPredit = self.FeedForward(input)
         accuracy = self.Evaluate(yPredit, target.T)
@@ -66,29 +68,24 @@ class NeuralNetworkClassifier:
                 delta.append(output_delta * self.transferFuncs[index](self._inputLayer[index], False))
             else:
                 delta_pullback = self.weights[index + 1].T.dot(delta[-1])
-                delta.append(delta_pullback[:, :] * self.transferFuncs[index](self._inputLayer[index], False))
+                delta.append(delta_pullback[:-1, :] * self.transferFuncs[index](self._inputLayer[index], False))
 
         "Update all the weights in the network"
         for index in range(self.layerCount):
             delta_index = self.layerCount - 1 - index
 
             if index == 0:
-                layerOutput = input.T
+                layerOutput = np.vstack([input.T, np.ones([1, nSamples])])
             else:
-                layerOutput = self._outputLayer[index - 1]
+                layerOutput = np.vstack([self._outputLayer[index - 1], np.ones([1, self._outputLayer[index - 1].shape[1]])])
 
             outputXdelta = layerOutput[None, :, :].transpose(2, 0, 1) * delta[delta_index][None, :, :].transpose(2, 1, 0)
             curDeltaWeight = np.sum(outputXdelta, axis = 0)
+            deltaWeight = (1-alpha*(lmbda/nSamples))*self.weights[index] - (alpha/lenMiniBatch) * curDeltaWeight #+ (beta/lenMiniBatch) * self._prevDeltaWeight[index]
 
-            deltaWeight = alpha * curDeltaWeight + beta * self._prevDeltaWeight[index]
+            self.weights[index] = deltaWeight
 
-            L1 = alpha * deltaWeight
-            L2 = alpha**2 * deltaWeight
-
-            self.weights[index] -= L1
-            self.weights[index] -= L2
-
-            self._prevDeltaWeight[index] = deltaWeight
+            #._prevDeltaWeight[index] = deltaWeight
 
         return accuracy
 
@@ -103,3 +100,18 @@ class NeuralNetworkClassifier:
         accuracy /= yActual.shape[0]
 
         return accuracy
+
+'''
+    "----- Update Mini Batch -----"
+    def UpdateMiniBatch(self, miniBatch, alpha):
+
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in miniBatch:
+            delta_nabla_b, delta_nabla_w = self.TrainingEpoch(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
+'''
--- src/NNCrossValidation.py
+++ src/NNCrossValidation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import utils
+import random
 import NNClassifier as nnClassifier
 
 trainingInput = np.load('standardized_train_inputs.npy')
@@ -7,16 +8,28 @@ trainingOutput = np.load('train_outputs.npy')
 
 nFeatures = trainingInput.shape[1]
 
-#trainingInput = trainingInput[:1000,:]
-#trainingOutput = trainingOutput[:1000]
+#trainingInput = trainingInput[:100,:]
+#trainingOutput = trainingOutput[:100]
+#for i in range(15):
+#    alpha, beta, nodes, nIter, nLayers = utils.RandomHyperParams(nFeatures)
+#    print("alpha = {0}, beta = {1}, nodes = {2}, nIter = {3}, nLayers = {4}".format(alpha, beta, nodes, nIter, nLayers))
+print("RUN 5...")
 
-alpha, beta, nodes, nIter, nLayers = utils.RandomHyperParams(nFeatures)
-print("alpha: {0}, beta: {1}, nodes: {2}, nIter: {3}, nLayers: {4}".format(alpha, beta, nodes, nIter, nLayers))
+lmbda = 5
+alpha = 0.1
+beta = 0.3
+nodes = 31
+nIter = 160
+nLayers = 2
+print("lmbda = {0}, alpha = {1}, nodes = {2}, nIter = {3}, nLayers = {4}".format(lmbda, alpha, nodes, nIter, nLayers))
 
 if nLayers == 1:
     nnc = nnClassifier.NeuralNetworkClassifier((nFeatures, nodes, 10))
+    print("nnc({0}, {1}, {2})".format(nFeatures, nodes, 10))
 else:
-    nnc = nnClassifier.NeuralNetworkClassifier((nFeatures, nodes, nodes, 10))
+    nodes2 = int(nodes / 2)
+    nnc = nnClassifier.NeuralNetworkClassifier((nFeatures, nodes, nodes2, 10))
+    print("nnc({0}, {1}, {2}, {3})".format(nFeatures, nodes, nodes2, 10))
 
 crossValid = utils.CrossValidation(trainingInput, trainingOutput, 5)
 
@@ -35,19 +48,23 @@ for kFold in range(5):
     trainingAccuracy = 0
     validationAccuracy = 0
 
-    for nIt in range(nIter+1):
-        for nBatchIndex in range(nTrainBatches):
-            n = nnc.nBatchSize * nBatchIndex
-            m = nnc.nBatchSize * (nBatchIndex + 1)
-            xTrainBatch = xTrain[n:m,:]
-            yTrainBatch = yTrain[n:m]
-            miniBatchAcc = nnc.Learning(xTrainBatch, yTrainBatch, alpha, beta)
+    xy = np.insert(xTrain, nFeatures, yTrain, axis = 1)
 
-        if nIt == nIter:
+    for nIt in range(nIter):
+        random.shuffle(xy)
+        miniBatches = [
+            xy[k : k+nnc.nBatchSize]
+            for k in range(0, nTrainBatches, nnc.nBatchSize)]
+        for miniBatch in miniBatches:
+            lenMiniBatch = len(miniBatch)
+            xMiniBatch = miniBatch[:, :-1]
+            yMiniBatch = miniBatch[:, -1]
+            miniBatchAcc = nnc.TrainingEpoch(xMiniBatch, yMiniBatch, lenMiniBatch, alpha, lmbda)#, beta)
+
+        if nIt == nIter - 1:
             trainingAccuracy = miniBatchAcc
             _trainingAccuracy.append(trainingAccuracy)
             print("--trainingAccuracy {0}".format(trainingAccuracy))
-
 
     yPredict = nnc.FeedForward(xValid)
     validationAccuracy = nnc.Evaluate(yPredict, yValid)
@@ -56,4 +73,24 @@ for kFold in range(5):
     print("--validationAccuracy {0}".format(validationAccuracy))
 
 print("Training Accuracy {0} : Validation Accuracy {1}".format((np.mean(_trainingAccuracy)),np.mean(_validationAccuracy)))
+
+
 #testInput = np.load('test_inputs_standardized.npy')
+'''
+        for nBatchIndex in range(nTrainBatches):
+            n = nnc.nBatchSize * nBatchIndex
+            m = nnc.nBatchSize * (nBatchIndex + 1)
+            xTrainBatch = xTrain[n:m,:]
+            yTrainBatch = yTrain[n:m]
+            miniBatchAcc = nnc.TrainingEpoch(xTrainBatch, yTrainBatch, alpha, beta)
+
+        if nIt == nIter:
+            trainingAccuracy = miniBatchAcc
+            _trainingAccuracy.append(trainingAccuracy)
+            print("--trainingAccuracy {0}".format(trainingAccuracy))
+
+        trainingAccuracy = nnc.TrainingEpoch(xTrain, yTrain, alpha, beta)
+        if nIt == nIter:
+            _trainingAccuracy.append(trainingAccuracy)
+            print("--trainingAccuracy {0}".format(trainingAccuracy))
+        '''
