f3f4724421a5ca223ef810658b254bfd89f34fa8
Stephanie Laflamme <stephanie.laflamme@mail.mcgill.ca>
2014-11-03 23:09:20 -0500

    Ben's script + plots

--- src/part2.py
+++ src/part2.py
@@ -11,86 +11,90 @@ print "Loading train output..."
 categories = loadnp("/Users/stephanielaflamme/Desktop/data_and_scripts/train_outputs.npy")
 
 print "Loading train input..."
-examples = loadnp("/Users/stephanielaflamme/Desktop/Numpy sets/train_inputs_standardized.npy")
+# examples = loadnp("/Users/stephanielaflamme/Desktop/Numpy sets/train_inputs_standardized.npy")
+train_input = loadnp("/Users/stephanielaflamme/Desktop/data_and_scripts/train_inputs.npy")
+test_input = loadnp("/Users/stephanielaflamme/Desktop/data_and_scripts/test_inputs.npy")
 
+print 'Standardizing...'
+scaler = preprocessing.StandardScaler().fit(train_input)
+examples = scaler.transform(train_input)
+test_data = scaler.transform(test_input)
 # ----------------
 # Loading test set
 # ----------------
 # print "Loading test input..."
-# test_data = loadnp("/Users/stephanielaflamme/Desktop/Numpy sets/test_inputs_%s.npy" %s)
+# test_data = loadnp("/Users/stephanielaflamme/Desktop/Numpy sets/test_inputs_standardized.npy")
 
 # ------------------------
 # Getting test predictions
 # ------------------------
-# classifier = Perceptron(alpha=0.0005, n_iter=25)
-# # train on the entire training set
-# classifier.train(examples, np.asarray(map(one_hot_vectorizer, categories)))
-# predictions = map(classifier.predict, test_data)
+print 'Getting test predictions'
+classifier = NeuralNet(layers= [2304, 25, 10], alpha=0.01, epochs=20)
+classifier.train(examples, np.asarray(map(one_hot_vectorizer, categories)))
+predictions = map(classifier.predict, test_data)
 
-# write_test_output(predictions)
+write_test_output(predictions)
 
-# ------------------------------
-# RANDOM SEARCH CROSS-VALIDATION
-# ------------------------------
+# # ------------------------------
+# # RANDOM SEARCH CROSS-VALIDATION
+# # ------------------------------
 
-print 'Beginning random search...'
+# print 'Beginning random search...'
 
-# the parameter values under consideration
-alphas = [0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001]
-n_layers = [1, 2]
-nodes_layer = [10, 15, 20, 25, 30]
-
-
-# this is where we'll save the cross-validation results
-# also note that we store the parameters we used!
-cross_val_alphas = []
-cross_val_layers = []
-cross_val_results = []
-cross_val_train_results = []
-cross_val_confusion_matrices = []
-
-predictions = []
-success_rates = []
-train_success_rates = []
+# # the parameter values under consideration
+# alphas = [0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001]
+# n_layers = [1, 2]
+# nodes_layer = [10, 15, 20, 25, 30]
 
 
-for _ in range(10):
-	alpha, layers = randomize_params(alphas, n_layers, nodes_layer)
+# # this is where we'll save the cross-validation results
+# # also note that we store the parameters we used!
+# cross_val_alphas = []
+# cross_val_layers = []
+# cross_val_results = []
+# cross_val_train_results = []
+# cross_val_confusion_matrices = []
 
-	cross_val_alphas.append(alpha)
-	cross_val_layers.append(layers)
+# predictions = []
+# success_rates = []
+# train_success_rates = []
+# for _ in range(10):
+# 	alpha, layers = randomize_params(alphas, n_layers, nodes_layer)
 
-	# do cross-validation with current parameters... use k=2 because it takes a long time to run
-	for data in CrossValidation(examples, categories, k=2):
-	    train_data, train_result, valid_data, valid_result = data
+# 	cross_val_alphas.append(alpha)
+# 	cross_val_layers.append(layers)
 
-	    # make and train classifier...
-	    classifier = NeuralNet(layers=layers, alpha=alpha, epochs=20)
-	    classifier.train(train_data, np.asarray(map(one_hot_vectorizer, train_result)))
+# 	# do cross-validation with current parameters... use k=2 because it takes a long time to run
+# 	for data in CrossValidation(examples, categories, k=2):
+# 	    train_data, train_result, valid_data, valid_result = data
 
-	    # get validation results for this fold
-	    guesses = map(classifier.predict, valid_data)
-	    correct = filter(lambda x: x[0] == x[1], zip(guesses, valid_result))
-	    ratio = len(correct)*1. / len(valid_result)
-	    success_rates.append(ratio)
+# 	    # make and train classifier...
+# 	    classifier = NeuralNet(layers=layers, alpha=alpha, epochs=20)
+# 	    classifier.train(train_data, np.asarray(map(one_hot_vectorizer, train_result)))
 
-	    predictions.extend(guesses)
+# 	    # get validation results for this fold
+# 	    guesses = map(classifier.predict, valid_data)
+# 	    correct = filter(lambda x: x[0] == x[1], zip(guesses, valid_result))
+# 	    ratio = len(correct)*1. / len(valid_result)
+# 	    success_rates.append(ratio)
 
-	# get the interesting results for this parameter configuration
-	confusion_matrix = get_confusion_matrix(categories, predictions)
-	success_ratio = sum(success_rates) / len(success_rates) * 100
+# 	    predictions.extend(guesses)
 
-	print 'Validation accuracy for alpha=%f, %d hidden layer(s) with layer sizes ' %(alpha, len(layers)-2), layers
-	print '     %f' % success_ratio
+# 	# get the interesting results for this parameter configuration
+# 	confusion_matrix = get_confusion_matrix(categories, predictions)
+# 	success_ratio = sum(success_rates) / len(success_rates) * 100
 
-	cross_val_confusion_matrices.append(confusion_matrix)
-	cross_val_results.append(success_ratio)
+# 	print 'Validation accuracy for alpha=%f, %d hidden layer(s) with layer sizes ' %(alpha, len(layers)-2), layers
+# 	print '     %f' % success_ratio
 
-	# save all the interesting results
-	np.save('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/results/nn_alphas_STEPH', cross_val_alphas)
-	np.save('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/results/nn_layers_STEPH', cross_val_layers)
-	np.save('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/results/nn_crossval_confmatrices_STEPH', cross_val_confusion_matrices)
-	np.save('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/results/nn_crossval_results_STEPH', cross_val_results)
+# 	cross_val_confusion_matrices.append(confusion_matrix)
+# 	cross_val_results.append(success_ratio)
+
+# 	# save all the interesting results
+# 	np.save('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/results/nn_alphas_STEPH', cross_val_alphas)
+# 	np.save('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/results/nn_layers_STEPH', cross_val_layers)
+# 	np.save('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/results/nn_crossval_confmatrices_STEPH', cross_val_confusion_matrices)
+# 	np.save('/Users/stephanielaflamme/Dropbox/COMP 598/Miniproject3/results/nn_crossval_results_STEPH', cross_val_results)
 
 
 
--- src/part3.py
+++ src/part3.py
@@ -1,23 +1,274 @@
+from sklearn.svm import SVC
+from sklearn import linear_model
+import numpy as np
+import random
+from sklearn import cross_validation
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from matplotlib.pylab import *
+import matplotlib.pyplot as plt
+from skimage.filter import gabor_kernel
+from scipy import ndimage
+from skimage.util import img_as_float
+import csv
+
+'''
+	Feature value = energy of response (sum of squares) aka Frobeniius norm
+'''
+def get_gabor_features(image, gaborKernels):
+	image = np.array(image)
+	#original_pixels = image.flat
+	features = []
+	#features.extend(original_pixels)
+	for i, k in enumerate(gaborKernels):
+		convimg = ndimage.convolve(image.reshape(48,48), k, mode='wrap')
+		features.extend(convimg.flat)
+
+		#features.append(convimg.mean())
+		#features.append(convimg.var())
+		#features.append(linalg.norm(convimg))
+		#features.append(linalg.norm(convimg, 1))
+	return features
+
+'''
+	Generates kernels to be used in feature extraction or as a kernel for SVM.
+	numTheta: int, number of thetas to use between 0,1 evenly spaced out
+	sigmaMin: int, Min sigma to consider (eg. 1)
+	sigmaMax: int, Max sigma to consider (eg. 3)
+	freqMin: float, min frequency to start off with (eg. 0.05)
+	freqMax: float, max frequency to end with (eg. 0.25)
+'''
+def getGaborKernels(n_theta = 4, sigmas=[1,3], frequencies=[0.05, 0.25]):
+	gaborKernels = []
+	for theta in range(n_theta):
+		theta = theta / float(n_theta) * np.pi
+		for sigma in sigmas:
+			for frequency in frequencies:
+				kernel = np.real(gabor_kernel(frequency, theta, sigma_x = sigma, sigma_y = sigma))
+				gaborKernels.append(kernel)
+	return gaborKernels
+
+'''
+	In our case, X should be the list of Ids so that images are not copied needlessly
+'''
+def splitValidTest(X, Y, validRatio):
+	X = X.tolist()
+	Y = Y.tolist()
+	valid = []
+	valid_y = []
+	length = len(X)
+	while (len(valid) < validRatio * length):
+		chosen_index = random.randint(0, len(X) - 1)
+		valid.append(X[chosen_index])
+		valid_y.append(Y[chosen_index])
+		del X[chosen_index]
+		del Y[chosen_index]
+	print "len valid: " + str(len(valid)) + "len Y" + str(len(valid_y))
+	print "len train: " + str(len(X)) + "len Y" + str(len(Y))
+	return (valid, X, valid_y, Y)
+
+def loadnp(filename):
+	data = np.load(filename)
+	return data
+
+def saveMatrix(mat):
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+
+	cax = ax.matshow(mat, cmap=cm.jet)
+	fig.colorbar(cax)
+
+	for x in xrange(10):
+		for y in xrange(10):
+			ax.annotate('%4.2f' % (mat[x][y]), xy=(y,x), horizontalalignment='center', verticalalignment='center', color='white')
+
+	plt.xticks(np.arange(10))
+	plt.yticks(np.arange(10))
+	ax.set_title('True label', fontsize=16)
+	ax.set_ylabel('Prediction', fontsize=16)
+
+	plt.savefig('SVM_gabor_confusion.pdf')
+
+def saveAccPlot(niters, valid_acc, train_acc, var):
+	fig = plt.figure()
+	ax = fig.add_subplot(1,1,1)
+
+	print niters
+	v, = ax.plot(niters, valid_acc, marker='D', color='red', label='Validation')
+	t, = ax.plot(niters, train_acc, marker='D', color='green', linestyle=':', label='Training')
+
+	# legend
+	handles, labels = ax.get_legend_handles_labels()
+	ax.legend(handles, labels, loc=0)
+
+	# labels
+	ax.set_xlabel(var)
+	ax.set_ylabel('Mean accuracy')
+	ax.set_xscale('log')
+	plt.savefig('SVM_standard_rate2.pdf')
+
+'''
+Done on the fly with the standard dataset because it is rather large
+'''
+def runKFoldWithGabor(numFolds, trainInput, trainOutput):
+	kfolds = cross_validation.StratifiedKFold(trainOutput, n_folds=numFolds)
+	conf_matrices = []
+	unique = np.unique(trainOutput)
+	kernels = getGaborKernels()
+
+	for train_index, test_index in kfolds:
+		(trainSet, validSet) = (trainInput[train_index], trainInput[test_index])
+		(trainSetY, validSetY) = (trainOutput[train_index], trainOutput[test_index])
+		print "len valid: " + str(len(validSet)) + "len Y" + str(len(validSetY))
+		print "len train: " + str(len(trainSet)) + "len Y" + str(len(trainSetY))
+		print "training SGD Classifier..."
+		data = []
+		data_y = []
+		counter = 0
+		clf = linear_model.SGDClassifier()
+
+		for i in xrange(len(trainSet)):
+			ex = trainSet[i]
+			f = get_gabor_features(ex, kernels)
+			data.append(f)
+			data_y.append(trainSetY[i])
+			if(len(data)%250 == 0 or i == len(trainSet) - 1):
+				counter = counter + 1
+				print str(len(data) * counter)
+				clf.partial_fit(data, data_y ,classes=unique)
+				data = []
+				data_y = []
+		data = []
+		print "checking against Valid Set..."
+		for i in xrange(len(validSet)):
+			ex = validSet[i]
+			f = get_gabor_features(ex, kernels)
+			data.append(f)
+		predictedY = clf.predict(data)
+		cMatrix = confusion_matrix(validSetY, predictedY)
+		cMatrix = cMatrix/cMatrix.astype(np.float).sum(axis=0)
+		conf_matrices.append(cMatrix)
+
+	final_matrix = np.mean(conf_matrices, axis=0)
+	saveMatrix(final_matrix)
+
+def runKFoldWithStandardTestIterations(numFolds, trainInput, trainOutput, numIterations):
+	kfolds = cross_validation.StratifiedKFold(trainOutput, n_folds=numFolds)
+	conf_matrices = []
+	accuracyV = []
+	accuracyT = []
+
+	for train_index, test_index in kfolds:
+		(trainSet, validSet) = (trainInput[train_index], trainInput[test_index])
+		(trainSetY, validSetY) = (trainOutput[train_index], trainOutput[test_index])
+		print "len valid: " + str(len(validSet)) + "len Y" + str(len(validSetY))
+		print "len train: " + str(len(trainSet)) + "len Y" + str(len(trainSetY))
+		print "training SGD Classifier..."
+		clf = linear_model.SGDClassifier(n_iter=numIterations)
+		clf.fit(trainSet, trainSetY)
+		print "checking against Valid Set..."
+		predictedY = clf.predict(validSet)
+		predX = clf.predict(trainSet)
+		cMatrix = confusion_matrix(validSetY, predictedY)
+		cMatrix = cMatrix/cMatrix.astype(np.float).sum(axis=0)
+		conf_matrices.append(cMatrix)
+		accV = accuracy_score(validSetY, predictedY)
+		accT = accuracy_score(trainSetY, predX)
+		accuracyV.append(accV)
+		accuracyT.append(accT)
+
+	final_matrix = np.mean(conf_matrices, axis=0)
+	saveMatrix(final_matrix)
+	return np.mean(accuracyV), np.mean(accuracyT)
+
+def runKFoldWithStandardTestLearningRate(numFolds, trainInput, trainOutput, l_rate):
+	kfolds = cross_validation.StratifiedKFold(trainOutput, n_folds=numFolds)
+	conf_matrices = []
+	accuracyV = []
+	accuracyT = []
+
+	for train_index, test_index in kfolds:
+		(trainSet, validSet) = (trainInput[train_index], trainInput[test_index])
+		(trainSetY, validSetY) = (trainOutput[train_index], trainOutput[test_index])
+		print "len valid: " + str(len(validSet)) + "len Y" + str(len(validSetY))
+		print "len train: " + str(len(trainSet)) + "len Y" + str(len(trainSetY))
+		print "training SGD Classifier..."
+		clf = linear_model.SGDClassifier(learning_rate='constant', eta0 = l_rate)
+		clf.fit(trainSet, trainSetY)
+		print "checking against Valid Set..."
+		predictedY = clf.predict(validSet)
+		predX = clf.predict(trainSet)
+		cMatrix = confusion_matrix(validSetY, predictedY)
+		cMatrix = cMatrix/cMatrix.astype(np.float).sum(axis=0)
+		conf_matrices.append(cMatrix)
+		accV = accuracy_score(validSetY, predictedY)
+		accT = accuracy_score(trainSetY, predX)
+		accuracyV.append(accV)
+		accuracyT.append(accT)
+
+	final_matrix = np.mean(conf_matrices, axis=0)
+	saveMatrix(final_matrix)
+	return np.mean(accuracyV), np.mean(accuracyT)
+
 # ------------------------
 # Getting test predictions using SVM and gabor features 
 # ------------------------
-#trainInput = loadnp("C:/Users/MicroMicro/Documents/Benjamin/Anaconda/Miniproject-3/src/train_inputs_gabor.npy")
-#trainOutput = loadnp("C:/Users/MicroMicro/Documents/Benjamin/Anaconda/Miniproject-3/src/train_outputs.npy")
+trainInput = loadnp('C:/Users/MicroMicro/Documents/Benjamin/Anaconda/Miniproject-3/src/train_inputs_standardized.npy')
+trainOutput = loadnp("C:/Users/MicroMicro/Documents/Benjamin/Anaconda/Miniproject-3/src/train_outputs.npy")
+testInput = loadnp("C:/Users/MicroMicro/Documents/Benjamin/Anaconda/Miniproject-3/src/test_inputs_standardized.npy")
 
-#print "lengths features: " + str(len(trainInput[0]))
-#print "lengths output: " + str(trainOutput[0])
+print "training SGD Classifier..."
+clf = linear_model.SGDClassifier()
+clf.fit(trainInput, trainOutput)
+predictedY = clf.predict(testInput)
+	
+test_output_file = open('test_output_SVM.csv', "wb")
+writer = csv.writer(test_output_file, delimiter=',') 
+writer.writerow(['Id', 'Prediction']) # write header
+id = 1
+for y in predictedY:
+	row = [id, y]
+	writer.writerow(row)
+	id = id + 1
+test_output_file.close()
 
-#(validSet, trainSet, validSetY, trainSetY) = splitValidTest(trainInput, trainOutput, 0.1)
-#print "done splitting... "
 
-#(validSetY, trainSetY) = fixOutputs(validSetY, trainSetY)
-#print validSetY
-#print trainSetY
+#let's run some experiments! First fix optimal learning rate and then vary iteration numbers
+#validaccs = []
+#trainaccs = []
+#iters = range(1,100)
 
-#print "Testing using pixel features... training..."
-#clf = SVC(kernel='linear')
-#clf.fit(trainSet, trainSetY)
+#for iter in iters:
+#	(v, t) = runKFoldWithStandardTestIterations(5, trainInput, trainOutput, iter)
+#	print "accuracy with " + str(iter) + " iterations is " + str(v)
+#	validaccs.append(v)
+#	trainaccs.append(t)
+#	with open("iterTestData.npy",'a') as f_handle:
+#		np.savetxt(f_handle, [v])
+#		np.savetxt(f_handle, [t])
 
-#print "checking against Valid Set..."
-#acc = clf.score(validSet, validSetY)
-#print acc
+#rates = []
+#counter = 0.000005
+#print rates
+#while counter < 0.0001:
+#	(v, t) = runKFoldWithStandardTestLearningRate(5, trainInput, trainOutput, counter)
+#	print "accuracy with " + str(counter) + " rate is " + str(v)
+#	validaccs.append(v)
+#	trainaccs.append(t)
+#	rates.append(counter)
+#	with open("rateTestData2.npy",'a') as f_handle:
+#		np.savetxt(f_handle, [v])
+#		np.savetxt(f_handle, [t])
+#	counter = counter + 0.000002
+
+#with open("rateTestData.npy", 'r') as f_handle:
+#	data = loadtxt(f_handle)
+#	counter = 0.0001
+#	for i in xrange(len(data)):
+#		if(i%2 == 0):
+#			validaccs.append(data[i])
+#			rates.append(counter)
+#			counter = counter + 0.0004
+#		else:
+#			trainaccs.append(data[i])
+#saveAccPlot(rates, validaccs, trainaccs, "Learning Rate")
